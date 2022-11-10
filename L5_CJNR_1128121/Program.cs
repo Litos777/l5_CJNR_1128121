9539e6f33bd66b13050e5042e65767f8a292907d
Carlos Navajas <Carlos Navajas@DESKTOP-DJO3KPK>
2022-11-09 22:44:02 -0600

    Agregar archivos de proyecto.

---
+++ L5_CJNR_1128121/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace L5_CJNR_1128121_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio No. 1");
+            Console.WriteLine("Ingrese un numero entero");
+            int gg;
+            gg = Convert.ToInt16(Console.ReadLine());
+            if (gg > 0)
+            {
+                Console.WriteLine("Es un numero positivo");
+            }
+            else if (gg < 0)
+            {
+                Console.WriteLine("Es un numero negativo");
+            }
+            else if (gg == 0)
+            {
+                Console.WriteLine("El numero es igual a 0");
+            }
+       
+
+        }
+    }
+}
